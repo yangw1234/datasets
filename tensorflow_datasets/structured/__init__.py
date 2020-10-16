d6b404d4e6845858fe686e52d73c454796ce6873
Constanza Fierro <constanzam@google.com>
2020-10-16 09:13:31 -0700

    Add Web NLG dataset
    PiperOrigin-RevId: 337517409

--- tensorflow_datasets/structured/__init__.py
+++ tensorflow_datasets/structured/__init__.py
@@ -26,4 +26,5 @@ from tensorflow_datasets.structured.movielens import Movielens
 from tensorflow_datasets.structured.radon import Radon
 from tensorflow_datasets.structured.rock_you import RockYou
 from tensorflow_datasets.structured.titanic import Titanic
+from tensorflow_datasets.structured.web_nlg import WebNlg
 from tensorflow_datasets.structured.wine_quality import WineQuality
